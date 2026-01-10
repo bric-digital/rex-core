72f3f7354e6d0f26951d54ec65142e90b8aa3dbb
ericka <emtrevino@gmail.com>
2026-01-09 21:08:07 -0600

    regex parsing error fixed in test

--- tests/specs/list-utilities.spec.js
+++ tests/specs/list-utilities.spec.js
@@ -277,9 +277,9 @@ test.describe('List Utilities - Pattern Matching', () => {
   test('should match subdomain wildcard pattern', async ({ page }) => {
     const results = await page.evaluate(() => {
       return {
-        exactMatch: window.ListUtilities.matchesPattern('https://google.com', '^https?://([a-z0-9-]+\\\\.)*google\\\\.com(/|$)', 'regex'),
-        subdomainMatch: window.ListUtilities.matchesPattern('https://mail.google.com', '^https?://([a-z0-9-]+\\\\.)*google\\\\.com(/|$)', 'regex'),
-        noMatch: window.ListUtilities.matchesPattern('https://facebook.com', '^https?://([a-z0-9-]+\\\\.)*google\\\\.com(/|$)', 'regex')
+        exactMatch: window.ListUtilities.matchesPattern('https://google.com', '^https?://([a-z0-9-]+\\.)*google\\.com(/|$)', 'regex'),
+        subdomainMatch: window.ListUtilities.matchesPattern('https://mail.google.com', '^https?://([a-z0-9-]+\\.)*google\\.com(/|$)', 'regex'),
+        noMatch: window.ListUtilities.matchesPattern('https://facebook.com', '^https?://([a-z0-9-]+\\.)*google\\.com(/|$)', 'regex')
       };
     });
 
